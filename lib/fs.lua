dd8b4d3191f87184b690b7ecef946d457db4afcb
DT <66632141+DT-is-not-available@users.noreply.github.com>
2023-03-08 13:26:33 -0500

    b

--- lib/fs.lua
+++ lib/fs.lua
@@ -1,4 +1,4 @@
-function exists(filepa)
+local function exists(filepa)
 	local file = table.concat(filepa:split("/"), "\\")
 	local ok, err, code = os.rename(file, file)
 	if not ok then
@@ -10,34 +10,52 @@ function exists(filepa)
 	return ok, err
 end
 
-function readFile(file)
-    local f = assert(io.open(file, "rb"))
-    local content = f:read("*all")
-    f:close()
-    return content
+local function readFile(file)
+	local f = assert(io.open(file, "rb"))
+	local content = f:read("*all")
+	f:close()
+	return content
 end
 
-function writeFile(file, data)
-    local f = assert(io.open(file, "w"))
+local function writeFile(file, data)
+	local f = assert(io.open(file, "w"))
 	f:write(data)
-    f:close()
+	f:close()
 end
 
-function fileSize(file)
+local function fileSize(file)
 	local f = assert(io.open(file, "r"))
 	local size = f:seek("end")
 	f:close()
 	return size
 end
 
-function getdir(directory)
-    local t, popen = {}, io.popen
-    local pfile = popen('dir /b /ad "'..directory..'"')
-    for filename in pfile:lines() do
-        t[#t+1] = filename
-    end
-    pfile:close()
-    return t
+local function getdir(directory)
+	local t, popen = {}, io.popen
+	local pfile = popen('dir /b /ad "'..directory..'"')
+	for filename in pfile:lines() do
+		t[#t+1] = filename
+	end
+	pfile:close()
+	return t
+end
+
+local function pack(tbl)
+	local data = ""
+	for key, value in pairs(tbl) do
+		data = data..love.data.pack('string', ">zI[2]", key, value)
+	end
+	return data
+end
+
+local function unpack(data)
+	local tbl = {}
+	local index = 1
+	local key, value
+	while index < #data do
+		key, value, index = love.data.unpack(">zI[2]", data, index)
+	end
+	return tbl
 end
 
 return {
@@ -45,5 +63,7 @@ return {
 	read=readFile,
 	write=writeFile,
 	size=fileSize,
-	getdir=getdir
+	getdir=getdir,
+	pack=pack,
+	unpack=unpack,
 }
